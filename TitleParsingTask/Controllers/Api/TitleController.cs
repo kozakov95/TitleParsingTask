e170db81153201cb61d29b28cf2c6304d3554e40
Kjes <stanriot@gmail.com>
2020-11-19 20:26:53 +0200

    Moved api\title endpoint to api TitleController

---
+++ TitleParsingTask/Controllers/Api/TitleController.cs
@@ -0,0 +1,25 @@
+﻿using HtmlAgilityPack;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TitleParsingTask.Models.Api.Title;
+using TitleParsingTask.Services;
+
+namespace TitleParsingTask.Controllers.Api
+{
+    public class TitleController:Controller
+    {
+        public TitleController()
+        {
+
+        }
+
+        [HttpPost("api/title")]
+        public async Task<IActionResult> GetTitle([FromBody] TitleRequestDto request, [FromServices]FetchTitleService titleService)
+        {
+            var responce = await titleService.FetchSiteDataAsync(request.Url);
+            return Ok(responce);
+        }
+    }
+}
--- TitleParsingTask/Controllers/HomeController.cs
+++ TitleParsingTask/Controllers/HomeController.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
-using HtmlAgilityPack;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using TitleParsingTask.Models;
 
 namespace TitleParsingTask.Controllers
 {
@@ -16,51 +8,14 @@ namespace TitleParsingTask.Controllers
         
         public HomeController()
         {
+
         }
 
         [HttpGet("")]
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
             return View();
         }
 
-        [HttpPost("api/title")]
-        public async Task<IActionResult> GetTitle([FromForm]string url)
-        {
-            url = url.ToLower();
-            url = url.Contains(":/") ? url : $"https://{url}";
-            var requestData = new ResponseData()
-            {
-                Url = url
-            };
-            try
-            {
-                var client = new HttpClient();
-                var response = await client.GetAsync(url);
-                var pageContents = await response.Content.ReadAsStringAsync();
-
-                var pageDocument = new HtmlDocument();
-                pageDocument.LoadHtml(pageContents);
-                var titleText = pageDocument.DocumentNode.SelectSingleNode("//title")?.InnerText ?? "<EMPTY>";
-
-                requestData.ResponseCode = ((int)response.StatusCode).ToString();
-                requestData.Title = titleText;
-            }
-            catch (HttpRequestException)
-            {
-                requestData.ResponseCode = "404";
-                requestData.Title = "<NOT_FOUND>";
-                return BadRequest(requestData);
-            }
-            catch(Exception)
-            {
-                requestData.ResponseCode = "Error";
-                requestData.Title = "Invalid error!";
-                return BadRequest(requestData);
-            }
-
-
-            return Ok(requestData);
-        }
     }
 }
